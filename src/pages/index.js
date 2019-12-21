915d82aad17e0d9a494578c31cac0014fb6d726d
Enochen <sirenochen@gmail.com>
2019-12-21 00:56:16 -0500

    playing around with possibilities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/about/">About</Link>
+    <Header headerText="This is a Header." />
+    <p>^-- THAT"S A HEADER!!1!</p>
+    <Header headerText="This is another Header - (should probably be a footer)" />
+  </div>
+)
