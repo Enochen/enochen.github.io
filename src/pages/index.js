c282dae40dd950cee1d6e5978fdb405659397068
Enochen <sirenochen@gmail.com>
2019-12-22 00:00:24 -0500

    add meme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Meme from "../images/meme.jpg"
 
 export default () => (
   <div style={{ color: `purple` }}>
     <Link to="/about/">About</Link>
     <Header headerText="This is a Header." />
     <p>^-- THAT"S A HEADER!!1!</p>
+    <img src={Meme} alt="Meme" />
     <Header headerText="This is another Header - (should probably be a footer)" />
   </div>
 )
