90b3979b24a028ad293d4f8ce143ad4437f7dd31
Enochen <sirenochen@gmail.com>
2019-12-22 21:58:20 -0500

    add component styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import Meme from "../images/meme.jpg"
+import Container from "../components/container"
 
 export default () => (
-  <div style={{ color: `purple` }}>
+  <Container>
     <Link to="/about/">About</Link>
     <Header headerText="This is a Header." />
     <p>^-- THAT"S A HEADER!!1!</p>
-    <img src={Meme} alt="Meme" />
+    <img src={Meme} alt="Meme" style={{height:"inherit",width:"inherit"}}/>
     <Header headerText="This is another Header - (should probably be a footer)" />
-  </div>
+  </Container>
 )
