857be97c4b474d3e960cde2bcb243f266fc75387
Enochen <sirenochen@gmail.com>
2019-12-26 22:49:57 -0500

    lint fix
    adds rel="noopener noreferrer" to target="_blank"

--- src/components/iconLink.js
+++ src/components/iconLink.js
@@ -3,7 +3,7 @@ import styles from "./iconLink.module.css"
 
 export default props => (
   <div className={styles.user}>
-    <a target="_blank" href={props.link}>
+    <a target="_blank" rel="noopener noreferrer" href={props.link}>
       <img src={props.icon} className={styles.icon} alt="" />
       <div className={styles.text}>{props.text}</div>
     </a>
