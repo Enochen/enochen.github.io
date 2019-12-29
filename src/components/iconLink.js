d5a8c9a0b3355da36be9f68de89a2c4f0c4a91e0
Enochen <sirenochen@gmail.com>
2019-12-28 22:31:51 -0500

    Change link styling

--- src/components/iconLink.js
+++ src/components/iconLink.js
@@ -4,8 +4,8 @@ import styles from "./iconLink.module.css"
 export default props => {
   const Icon = props.icon
   return (
-    <div className={styles.user}>
-      <a target="_blank" rel="noopener noreferrer" href={props.link}>
+    <div className={styles.wrapper}>
+      <a className={styles.link} target="_blank" rel="noopener noreferrer" href={props.link}>
         <Icon size={42} />
         <div className={styles.text}>{props.text}</div>
       </a>
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,16 @@
 import Typography from "typography"
 import moraga from "typography-theme-moraga"
 moraga.overrideThemeStyles = ({ rhythm }, options) => ({
-  'h1,h2': {
-    marginBottom: rhythm(1/3)
-  }
+  "h1,h2": {
+    marginBottom: rhythm(1 / 3),
+  },
+  a: {
+    color: "hsla(0,0%,0%,0.7)",
+  },
+  "a:hover": {
+    color: "#8c89cc",
+    textDecoration: "none",
+  },
 })
 const typography = new Typography(moraga)
 export const { scale, rhythm, options } = typography
