bf65923050efb9bb916a91bdc34d66fe5e961add
Enochen <sirenochen@gmail.com>
2019-12-28 23:19:14 -0500

    Restructure components

--- src/components/iconLink.js
+++ src/components/iconLink.js
@@ -3,9 +3,15 @@ import styles from "./iconLink.module.css"
 
 export default props => {
   const Icon = props.icon
+  const ext = props.external
+    ? {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      }
+    : {}
   return (
     <div className={styles.wrapper}>
-      <a className={styles.link} target="_blank" rel="noopener noreferrer" href={props.link}>
+      <a className={styles.link} href={props.link} {...ext}>
         <Icon size={42} />
         <div className={styles.text}>{props.text}</div>
       </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,47 @@ import Header from "../components/nameHeader"
 import Section from "../components/section"
 import IconLink from "../components/iconLink.js"
 import IconGroup from "../components/iconGroup"
-import { IconContext } from "react-icons";
-import { FaGithub, FaNewspaper, FaLinkedin, FaEnvelope, FaHammer } from 'react-icons/fa';
+import {
+  FaGithub,
+  FaNewspaper,
+  FaLinkedin,
+  FaUser,
+  FaFlask,
+} from "react-icons/fa"
 
-export default () => (
+const Heading = () => (
   <Section>
-    <Header heading="Enoch Chen" subHeading="CS @ Cornell" />
+    <Header heading="Enoch Chen" subHeading="cs @ cornell" />
     <IconGroup>
-      <IconLink
-        text="Projects"
-        icon={FaHammer}
-        link="#"
-      />
+      <IconLink text="About" icon={FaUser} link="#about" />
+      <IconLink text="Projects" icon={FaFlask} link="#projects" />
       <IconLink
         text="Github"
         icon={FaGithub}
         link="http://github.com/Enochen"
+        external={true}
       />
       <IconLink
         text="LinkedIn"
         icon={FaLinkedin}
         link="https://www.linkedin.com/in/enochen728/"
+        external={true}
       />
       <IconLink
         text="Resume"
         icon={FaNewspaper}
         link="/Enoch Chen cv.pdf"
+        external={true}
       />
     </IconGroup>
   </Section>
+)
+
+const Projects = () => <div id="projects"></div>
+
+export default () => (
+  <>
+    <Heading />
+    <Projects />
+  </>
 )
