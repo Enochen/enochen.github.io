673bdbd74521102d88d1ef439afbc3cc3251c026
Enochen <sirenochen@gmail.com>
2020-01-07 02:05:58 -0500

    scrolling hack

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
-require("typeface-source-sans-pro");
+const scrollTo = (id) => () => {
+  const el = document.querySelector(id)
+  if (el) return window.scrollTo(0, el.offsetTop - 20)
+  return false
+}
+
+export const onRouteUpdate = ({ location: { hash } }) => {
+  if (hash) {
+    window.setTimeout(scrollTo(hash), 10)
+  }
+}
