296cfda775db360cc49a343c977a1dfe9581351a
Enoch Chen <sirenochen@gmail.com>
2020-02-03 00:15:19 -0500

    minor tidying-up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,3 @@
 export const onInitialClientRender = () => {
   require("typeface-source-sans-pro")
 }
-
-// const scrollTo = id => () => {
-//   const el = document.querySelector(id)
-//   if (el) return window.scrollTo(0, el.offsetTop)
-//   return false
-// }
-
-// export const onRouteUpdate = ({ location: { hash } }) => {
-//   if (hash) {
-//     //window.setTimeout(scrollTo(hash), 0)
-//   }
-// }
